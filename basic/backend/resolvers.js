91e6d33f18663a0ff30e0971669b75ec235ba6a4
tpiros <2042718+tpiros@users.noreply.github.com>
2020-01-23 14:39:19 +0100

    adding api & backend

---
+++ basic/backend/resolvers.js
@@ -0,0 +1,25 @@
+const { paginateResults } = require('./utils');
+
+module.exports = {
+  Query: {
+    cars: async (_, { pageSize = 500, after }, { dataSources }) => {
+      const allCars = await dataSources.carAPI.getCars();
+
+      const cars = paginateResults({
+        after,
+        pageSize,
+        results: allCars
+      });
+
+      return {
+        cars,
+        cursor: cars.length ? cars[cars.length - 1].cursor : null,
+        hasMore: cars.length ? cars[cars.length - 1].cursor !== allCars[allCars.length - 1].cursor : false
+      }
+    },
+    car: (_, { id }, { dataSources }) => dataSources.carAPI.getCar({ id })
+    
+  }
+};
+
+// cars: (_, __, { dataSources }) => dataSources.carAPI.getCars(),
